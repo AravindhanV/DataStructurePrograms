8e250245a5bfd6df0f933f9abc1ac694bd23cfab
Aravindhan V <aravindhan.v@perfios.com>
2021-09-03 18:46:16 +0530

    ADDED: program to check if brackets are balanced in an expresion

--- src/com/bridgelabz/datastructureprograms/BalancedBrackets.java
+++ src/com/bridgelabz/datastructureprograms/BalancedBrackets.java
@@ -1,7 +1,29 @@
 package com.bridgelabz.datastructureprograms;
 
+import java.util.Scanner;
+
+import com.bridgelabz.datastructures.Stack;
+
 public class BalancedBrackets {
 	public static void main(String[] args) {
-
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Enter the expression");
+		String expression = scanner.nextLine();
+		Stack<Character> stack = new Stack();
+		for(int index=0;index<expression.length();index++) {
+			if(expression.charAt(index)=='(') {
+				stack.push('(');
+			} else if(expression.charAt(index) == ')') {
+				int result = stack.pop();
+				if(result == -1) {
+					System.out.println("Brackets not balanced");
+				}
+			}
+		}
+		if(stack.isEmpty()) {
+			System.out.println("Balanced brackets");
+		} else {
+			System.out.println("Brackets not balanced");
+		}
 	}
 }
